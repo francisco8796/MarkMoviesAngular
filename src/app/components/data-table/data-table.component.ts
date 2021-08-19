85dbeb21aedd9dae9fc254251c955b64518114d8
francisco8796 <87489733+francisco8796@users.noreply.github.com>
2021-08-19 09:55:53 +0100

    Finishing the infinite scroll

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -8,40 +8,60 @@ import { MovieDataService } from 'src/app/services/movie-data.service';
 export class DataTableComponent implements OnInit {
 
   constructor(public moviedata: MovieDataService) { }
+  //Lista com todos os dados
   movieList: any;
+  //lista com os filmes
   movies: any;
-  src = '../../../assets/eye.svg';
+  //Numero da pagina
+  page: number = 0;
+  //Tamanho da pagina
+  size: number = 15;
+  //Mostrar/esconder os detalhes especificos do filme
   detailsIsVisible: boolean = false;
+  //imagem
+  src = '../../../assets/eye.svg';
   @Output() value: string = "";
 
-  page=5;
-  size=11;
 
-  onScroll(event: any) {
-    // visible height + pixel scrolled >= total height 
-    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
-      this.page+=1;
-      console.log(this.page);
-    }
-}
+
+
   ngOnInit(): void {
+    //carregar os filmes 
+    this.loadInitPost();
+  }
+
+  loadInitPost() {
     //Ir buscar o get ao serviço 
-    this.moviedata.getMovies().subscribe(data => {
+    this.moviedata.getMoviesWithPages(this.page, this.size).subscribe((data: any) => {
       //todos os dados da api
       this.movieList = data;
       //Apenas dados que é preciso 
-      this.movies = this.movieList.content;
-      console.log(this.movies);
-    });    
+      this.movies = this.movieList.content
+    });
   }
 
+  loadNextPost() {
+    //aumentar o tamanho da pagina
+    this.size += 2;
+    //Ir buscar o get ao serviço 
+    this.moviedata.getMoviesWithPages(this.page, this.size)
+      .subscribe((data: any) => {
+        //todos os dados da api
+        this.movieList = data;
+        //Apenas dados que é preciso 
+        this.movies = this.movieList.content
+      });
+  }
+  onScroll() {
+    //carregar o proximo filme
+    this.loadNextPost();
+  }
 
   showDetails(id: string) {
     //Mostrar os detalhes especificos aka "more details"
     this.detailsIsVisible = this.detailsIsVisible ? false : true;
     //passar o id para valor que depois o valor é enviado para os "more details"
     this.value = id;
-    console.log(this.value);
   }
 
 
